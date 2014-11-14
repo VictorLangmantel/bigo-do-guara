bf06edc90e010d43d9493b0c19ecaaa31ee2f4a6
gabriellangmantel@hotmail.com <gabriellangmantel@hotmail.com@19e1c79c-5d0d-e048-d979-3f80bda7454c>
2014-11-14 18:13:41 +0000

    mcdonalds

---
+++ trunk/Bebida.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mcdonalds;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Bebida {
+    String nome;
+    Double preco;
+    Integer codigo;
+    Double qtd;
+   Double unidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Double getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(Double qtd) {
+        this.qtd = qtd;
+    }
+
+    public Double getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(Double unidade) {
+        this.unidade = unidade;
+    }
+    public String descricaocardapio (){
+        return nome + "..." + preco;
+    }
+}
