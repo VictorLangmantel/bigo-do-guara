87453e4ba83e9356c3228ec28ca74b071743c74e
gabriellangmantel@hotmail.com <gabriellangmantel@hotmail.com@19e1c79c-5d0d-e048-d979-3f80bda7454c>
2014-11-14 18:16:34 +0000

    Mcdonalds

---
+++ trunk/Produto.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mcdonalds;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Produto {
+  Double preco;
+  String nome;
+  Integer codigo;
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+public String descricaocardapio(){
+return nome + "..." + preco;    
+}  
+}
